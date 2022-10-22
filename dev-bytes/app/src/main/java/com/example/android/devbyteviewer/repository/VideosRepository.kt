8540d33a8b3182150dfb59af4b45aae3ac799c86
Felopater Adel <fa1458@fayoum.edu.eg>
2022-10-22 15:44:54 +0200

    complete Project dev-byte(offline caching)

--- dev-bytes/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
+++ dev-bytes/app/src/main/java/com/example/android/devbyteviewer/repository/VideosRepository.kt
@@ -16,3 +16,29 @@
  */
 
 package com.example.android.devbyteviewer.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.android.devbyteviewer.database.VideosDatabase
+import com.example.android.devbyteviewer.database.asDomainModel
+import com.example.android.devbyteviewer.domain.Video
+import com.example.android.devbyteviewer.network.Network
+import com.example.android.devbyteviewer.network.asDatabaseModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class VideosRepository(private val database: VideosDatabase)
+{
+    val video: LiveData<List<Video>> = Transformations.map(database.videoDao.getVideo()){
+        it.asDomainModel()
+    }
+    suspend fun refreshVideo()
+    {
+        withContext(Dispatchers.IO)
+        {
+            val playlist = Network.devbytes.getPlaylist().await()
+            database.videoDao.insertAll(*playlist.asDatabaseModel())
+
+        }
+    }
+}
--- dev-bytes/app/src/main/java/com/example/android/devbyteviewer/ui/DevByteActivity.kt
+++ dev-bytes/app/src/main/java/com/example/android/devbyteviewer/ui/DevByteActivity.kt
@@ -17,6 +17,7 @@
 
 package com.example.android.devbyteviewer.ui
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.android.devbyteviewer.R
@@ -25,13 +26,15 @@ import com.example.android.devbyteviewer.R
  * This is a single activity application that uses the Navigation library. Content is displayed
  * by Fragments.
  */
-class DevByteActivity : AppCompatActivity() {
+class DevByteActivity : AppCompatActivity()
+{
 
     /**
      * Called when the activity is starting.  This is where most initialization
      * should go
      */
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dev_byte_viewer)
     }
