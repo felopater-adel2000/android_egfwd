ae45e727f353c9855b63a039e9e8cdfab48e2885
Felopater Adel <fa1458@fayoum.edu.eg>
2022-11-05 00:24:39 +0200

    modify notification app

--- notifications/app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
+++ notifications/app/src/main/java/com/example/android/eggtimernotifications/MyFirebaseMessagingService.kt
@@ -1,6 +1,9 @@
 package com.example.android.eggtimernotifications
 
+import android.app.NotificationManager
 import android.util.Log
+import androidx.core.content.ContextCompat
+import com.example.android.eggtimernotifications.util.sendNotification
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -10,11 +13,32 @@ class MyFirebaseMessagingService : FirebaseMessagingService()
     {
         Log.i("Felo", "Refresh Token: $token")
 
-        //sendRegistrationToServer(token)
+        sendRegistrationToServer(token)
+    }
+
+    private fun sendRegistrationToServer(token: String?) {
+
     }
 
     override fun onMessageReceived(message: RemoteMessage?)
     {
+        Log.i("Felo", "From: ${message?.from}")
 
+        message?.data.let {
+            Log.i("Felo", "data: ${message?.data}")
+        }
+
+
+        message?.notification?.let {
+            Log.d("Felo", "Message Notification Body: ${it.body}")
+            sendNotification(it.body!!)
+        }
+    }
+
+    private fun sendNotification(body: String)
+    {
+        val notificationManager = ContextCompat.getSystemService(applicationContext, NotificationManager::class.java) as NotificationManager
+
+        notificationManager.sendNotification(body, applicationContext)
     }
 }
