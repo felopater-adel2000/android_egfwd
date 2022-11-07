c8a557e66ec89ae16bc4caa59125493c8d96d18e
Felopater Adel <fa1458@fayoum.edu.eg>
2022-11-07 13:31:39 +0200

    adding MyCanvasView to Screen

--- mini-paint/app/src/main/java/com/example/minipaint/MainActivity.kt
+++ mini-paint/app/src/main/java/com/example/minipaint/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.minipaint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //create variable of MyCanvasView
+        val myCanvasView = MyCanvasView(this)
+
+        //set myCanvasView to full screen
+        myCanvasView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        //adding content description to myCanvasView
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+
+        setContentView(myCanvasView)
+
     }
 }
