e7fb31816bd26f22c5a571b86133087a5c790cba
Felopater Adel <fa1458@fayoum.edu.eg>
2022-11-18 15:31:21 +0200

    send Notification when geofence enter

--- Geofences/app/src/main/java/com/example/android/treasureHunt/GeofenceBroadcastReceiver.kt
+++ Geofences/app/src/main/java/com/example/android/treasureHunt/GeofenceBroadcastReceiver.kt
@@ -36,8 +36,44 @@ import com.google.android.gms.location.GeofencingEvent
  */
 class GeofenceBroadcastReceiver : BroadcastReceiver() {
 
-    override fun onReceive(context: Context, intent: Intent) {
+    override fun onReceive(context: Context, intent: Intent)
+    {
         // TODO: Step 11 implement the onReceive method
+        if (intent.action == ACTION_GEOFENCE_EVENT)
+        {
+            val geofencingEvent = GeofencingEvent.fromIntent(intent)
+
+            if(geofencingEvent.hasError())
+            {
+                val errorMessage = errorMessage(context, geofencingEvent.errorCode)
+                Log.i("Felo", errorMessage)
+                return
+            }
+
+            if(geofencingEvent.geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER)
+            {
+                Log.i("Felo", context.getString(R.string.geofence_entered))
+                val fenceId = when{
+                    geofencingEvent .triggeringGeofences.isNotEmpty() -> geofencingEvent.triggeringGeofences[0].requestId
+                    else -> {
+                        Log.i("Felo", "No Geofence Trigger Found! Abort mission!")
+                        return
+                    }
+                }
+
+                val foundIndex = GeofencingConstants.LANDMARK_DATA.indexOfFirst { it.id == fenceId }
+
+                if(-1 == foundIndex)
+                {
+                    Log.i("Felo", "Unknown Geofence Abort Mission!")
+                    return
+                }
+                val notificationManager: NotificationManager = ContextCompat.getSystemService(context, NotificationManager::class.java) as NotificationManager
+
+                notificationManager.sendGeofenceEnteredNotification(context, foundIndex)
+
+            }
+        }
     }
 }
 
