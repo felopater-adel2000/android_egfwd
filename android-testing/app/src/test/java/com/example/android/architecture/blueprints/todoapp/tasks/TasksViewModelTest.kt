93e3cfed2decaec33eb55a3ab5bf8520d51487db
Felopater Adel <62791172+felopater-adel2000@users.noreply.github.com>
2022-11-26 13:03:36 +0200

    update ViewModel test and add ViewModel Factory

--- android-testing/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ android-testing/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -4,6 +4,9 @@ import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.android.architecture.blueprints.todoapp.*
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.DefaultTasksRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
 import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.CoreMatchers.*
 import org.hamcrest.MatcherAssert.assertThat
@@ -13,9 +16,10 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 //when run with AndroidJUnit4 it use Androidx to to choose test method local test or Instrumented
-@RunWith(AndroidJUnit4::class)
+//@RunWith(AndroidJUnit4::class)
 class TasksViewModelTest
 {
+    private lateinit var tasksRepository: FakeTestRepository
     //this line run all background tasks for android before run test
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
@@ -25,7 +29,12 @@ class TasksViewModelTest
     @Before
     fun setupViewModel()
     {
-        tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
+        tasksRepository = FakeTestRepository()
+        val task1 = Task("Title1", "Description1")
+        val task2 = Task("Title2", "Description2", true)
+        val task3 = Task("Title3", "Description3", true)
+        tasksRepository.addTask(task1, task2, task3)
+        tasksViewModel = TasksViewModel(tasksRepository)
     }
 
     @Test
