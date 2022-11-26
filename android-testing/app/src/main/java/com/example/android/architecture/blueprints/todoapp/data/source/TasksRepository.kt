96845eb0bdc950742b5e1dcc39be15eb0ee22489
Felopater Adel <62791172+felopater-adel2000@users.noreply.github.com>
2022-11-26 12:47:38 +0200

    write FakeTasksRepository

---
+++ android-testing/app/src/main/java/com/example/android/architecture/blueprints/todoapp/data/source/TasksRepository.kt
@@ -0,0 +1,46 @@
+package com.example.android.architecture.blueprints.todoapp.data.source
+
+import androidx.lifecycle.LiveData
+import com.example.android.architecture.blueprints.todoapp.data.Result
+import com.example.android.architecture.blueprints.todoapp.data.Task
+
+interface TasksRepository {
+    /** we get remote and local Data source from constructor so we do not need init */
+//    init {
+//        val database = Room.databaseBuilder(application.applicationContext,
+//            ToDoDatabase::class.java, "Tasks.db")
+//            .build()
+//
+//        tasksRemoteDataSource = TasksRemoteDataSource
+//        tasksLocalDataSource = TasksLocalDataSource(database.taskDao())
+//    }
+
+    suspend fun getTasks(forceUpdate: Boolean = false): Result<List<Task>>
+
+    suspend fun refreshTasks()
+    fun observeTasks(): LiveData<Result<List<Task>>>
+
+    suspend fun refreshTask(taskId: String)
+    fun observeTask(taskId: String): LiveData<Result<Task>>
+
+    /**
+     * Relies on [getTasks] to fetch data and picks the task with the same ID.
+     */
+    suspend fun getTask(taskId: String, forceUpdate: Boolean = false): Result<Task>
+
+    suspend fun saveTask(task: Task)
+
+    suspend fun completeTask(task: Task)
+
+    suspend fun completeTask(taskId: String)
+
+    suspend fun activateTask(task: Task)
+
+    suspend fun activateTask(taskId: String)
+
+    suspend fun clearCompletedTasks()
+
+    suspend fun deleteAllTasks()
+
+    suspend fun deleteTask(taskId: String)
+}
---
+++ android-testing/app/src/test/java/com/example/android/architecture/blueprints/todoapp/data/source/FakeTestRepository.kt
@@ -0,0 +1,84 @@
+package com.example.android.architecture.blueprints.todoapp.data.source
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.android.architecture.blueprints.todoapp.data.Result
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import kotlinx.coroutines.test.runBlockingTest
+
+class FakeTestRepository : TasksRepository
+{
+    var tasksServiceData: LinkedHashMap<String, Task> = LinkedHashMap()
+
+    private val observableTask = MutableLiveData<com.example.android.architecture.blueprints.todoapp.data.Result<List<Task>>>()
+
+    override suspend fun getTasks(forceUpdate: Boolean): Result<List<Task>>
+    {
+        return com.example.android.architecture.blueprints.todoapp.data.Result.Success(tasksServiceData.values.toList())
+    }
+
+    override suspend fun refreshTasks()
+    {
+        observableTask.value = getTasks()
+    }
+
+    override fun observeTasks(): LiveData<Result<List<Task>>>
+    {
+        runBlockingTest { refreshTasks() }
+        return observableTask
+    }
+
+    override suspend fun refreshTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun observeTask(taskId: String): LiveData<Result<Task>> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun getTask(taskId: String, forceUpdate: Boolean): Result<Task> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun saveTask(task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun completeTask(task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun completeTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun activateTask(task: Task) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun activateTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun clearCompletedTasks() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteAllTasks() {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun deleteTask(taskId: String) {
+        TODO("Not yet implemented")
+    }
+
+    fun addTask(vararg tasks: Task)
+    {
+        for(task in tasks)
+        {
+            tasksServiceData[task.id] = task
+        }
+
+        runBlockingTest {refreshTasks()}
+    }
+}
