8b7019f018be2441739b959e25f76102fecc8dfb
Felopater Adel <62791172+felopater-adel2000@users.noreply.github.com>
2022-11-28 18:04:13 +0200

    pause and resume coroutine dispatcher

---
+++ android-testing/app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModelTest.kt
@@ -0,0 +1,45 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.example.android.architecture.blueprints.todoapp.MainCoroutineRule
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeTestRepository
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@ExperimentalCoroutinesApi
+class StatisticsViewModelTest
+{
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var tasksRepository: FakeTestRepository
+    private lateinit var statisticsViewModel: StatisticsViewModel
+
+    @Before
+    fun initFakeTestRepository()
+    {
+        tasksRepository = FakeTestRepository()
+        statisticsViewModel = StatisticsViewModel(tasksRepository)
+    }
+
+    @Test
+    fun loadTasks_loading()
+    {
+        mainCoroutineRule.pauseDispatcher()
+        statisticsViewModel.refresh()
+
+        assertThat(statisticsViewModel.dataLoading.getOrAwaitValue(), `is`(true))
+        mainCoroutineRule.resumeDispatcher()
+
+        assertThat(statisticsViewModel.dataLoading.getOrAwaitValue(), `is`(false))
+    }
+}
